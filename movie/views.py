a16aa9b2152ba4622d2399532618c8b03f115c4d
Tomás Giraldo <Tgiraldoo@eafit.edu.co>
2025-07-30 09:40:50 -0500

    primer commit

---
+++ movie/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return HttpResponse("This is the About Page")
